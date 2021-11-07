a0ba096b1451b31f406f5478f72cba92a8d056fe
Mihail Alekseev <mihailalekseev02@gmail.com>
2021-11-07 11:02:01 +0200

    Create properties for tests that catch all bogus heaps except 2

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -1,20 +1,76 @@
 package quickcheck
 
-import org.scalacheck._
-import Arbitrary._
-import Gen._
-import Prop._
-
+import org.scalacheck.*
+import Arbitrary.*
+import Gen.*
+import Prop.*
 import quickcheck.IntHeap
+import scala.annotation.tailrec
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
-  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+  property("findMin on two elements returns fewer") = forAll { (n: Int, m: Int) =>
+    val heap = insert(n, insert(m, empty))
 
-  property("gen1") = forAll { (h: H) =>
+    val min = findMin(heap)
+
+    if (n <= m) min == n
+    else min == m
+  }
+
+  property("insert and deleteMin make heap become empty") = forAll { (n: Int) =>
+    val heap = insert(n, empty)
+
+    val emptyH = deleteMin(heap)
+
+    isEmpty(emptyH)
+  }
+
+  property("insert and deleteMin n elements make heap become empty") = forAll { (n: List[Int]) =>
+    @tailrec
+    def insertElements(l: List[Int], h: H): H =
+      if (l.isEmpty) h
+      else insertElements(l.tail, insert(l.head, h))
+
+    @tailrec
+    def deleteCertainElements(h: H, number: Int): H =
+      if (number == 0) h
+      else deleteCertainElements(deleteMin(h), number - 1)
+
+    val heap = insertElements(n, empty)
+
+    val emptyH = deleteCertainElements(heap, n.length)
+
+    isEmpty(emptyH)
+  }
+
+  property("isSorted always returns true") = forAll { (a: H) =>
+    def isSorted(min: Int, h: H): Boolean =
+      if (isEmpty(h)) true
+      else if ( min > findMin(h) ) false
+      else isSorted(findMin(h), deleteMin(h))
+
+    isSorted(findMin(a), deleteMin(a))
+  }
+
+  property("findMin on melded two heaps returns min element of them") = forAll { (a: H, b: H) =>
+    val heap = meld(a, b)
+
+    val minMelded = findMin(heap)
+
+    findMin(a) == minMelded || findMin(b) == minMelded
+  }
+
+  property("!isEmpty then findMin and insert result to heap, findMin must be equal to previous findMin") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
+
     findMin(insert(m, h)) == m
   }
 
+  lazy val genHeap: Gen[H] = for {
+    i <- arbitrary[Int]
+    gH <- frequency( (20, const(empty)), (80, genHeap) )
+  } yield insert(i, gH)
+
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 }
